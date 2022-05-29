0b150b6b18bee443e5dc6475d7a0497c685d333c
iPhil <philippmielke@icloud.com>
2022-05-29 22:47:56 +0200

    versuche xx

--- src/static_webserver.py
+++ src/static_webserver.py
@@ -19,8 +19,6 @@ class Application(tornado.web.Application):
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         # schreibe Webseitenkontent
-        #pathPicture = get_camListValue('browser')
-        #pathPicture = pathPicture.path + pathPicture.name + '.jpg'
         self.render("upload.html")
 
 def write_webpage() -> str:
@@ -33,21 +31,22 @@ def write_webpage() -> str:
     page = "<!DOCTYPE html>\
             <html>\
             <head>\
-            <title>Embedded Systems Projektarbeit</title>\
+            <title>\"Embedded Systems Projektarbeit\"</title>\
             </head>\
             <body>\
-            <center><h1>Embedded Systems Projektarbeit</h1></center>\
+            <center><h1>\"Embedded Systems Projektarbeit\"</h1></center>\
             <center>" + content + "</center>\
             <center>" + timeinfo + "</center>\
             </body>\
             </html>"
-    with open('upload.html', 'w') as f:
+    with open('./src/upload.html', 'w') as f:
         f.write(page)
 
 # verbinde MainHandler mit App
 def start_static_webserver():
     write_webpage()
     try:
+        """
         applicaton = Application()
         http_server = tornado.httpserver.HTTPServer(applicaton)
         http_server.listen(8081)
@@ -55,18 +54,21 @@ def start_static_webserver():
         #tornado.ioloop.IOLoop.instance().start()
         tornado.ioloop.IOLoop.current().start()
 
-        """
+
         browser = get_camListValue('browser')
         #static_path = os.path.join(os.path.dirname(__file__),"static"))
         app = tornado.web.Application([
             (   r'/', MainHandler),
             (   r'/(.*)', \
                tornado.web.StaticFileHandler, \
-               {'path':r'/home/phil/Uniprojekte/'}),
+               {'path':r'/home/phil/Uniprojekte/Lab4'}),
         ])
+        """
+        applicaton = Application()
+        app = tornado.web.Application(applicaton)
         app.listen(8081)
         tornado.ioloop.IOLoop.current().start()
-        """
+
     except KeyboardInterrupt:
         tornado.ioloop.IOLoop.current().stop()
         #tornado.ioloop.IOLoop.instance().stop()
